760a6dba088b20dc08f8fd89e8ff403ee2aa9dec
Muhammadkhon <muhammadkhonabdulloev@gmail.com>
2020-12-22 01:03:03 +0500

    Some FIX

--- docker-entrypoint-initdb.d/data.sql
+++ docker-entrypoint-initdb.d/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO managers (name, phone, password, is_admin)
-VALUES ('vasya', '+992000000001', 'Secret', true);
+VALUES ('vasya', '+992000000001', '$2a$10$OaUtjCNv2DT5x/dXcV.P3eYkIPIRtBr/v8Nluwifz6brSkfyXOh6m', true);
